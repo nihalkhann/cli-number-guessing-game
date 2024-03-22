710b6f92fcb1d4bf8fbe9e45b1bd7cced01c611b
nihalkhann <144270394+nihalkhann@users.noreply.github.com>
2024-03-23 02:18:23 +0500

    create a number guessing game

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimations from "chalk-animation";
+async function welcome() {
+    let rainbowTitle = chalkAnimations.karaoke("Number Guessing game");
+}
+welcome();
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const asnwers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: chalk.blueBright("please guess a number between 1 to 6"),
+    },
+]);
+if (asnwers.userGuessNumber === randomNumber) {
+    console.log(chalk.blue("Congratulation you guess a number"));
+}
+else {
+    console.log(chalk.red("sorry wrong guess"));
+}
